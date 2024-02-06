de1e77a5b8b2aeb47f1ce4879945a6c2e620ed33
shubham <vickygaikwad41996@gmail.com>
2024-02-07 01:13:16 +0530

    REQ0001 added custom security group called demo-sg with ssh

--- terraform_code/V1-ec2.tf
+++ terraform_code/V1-ec2.tf
@@ -6,8 +6,35 @@ resource "aws_instance" "demo-server" {
   ami = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name = "Key"
+  security_groups = [ "demo-sg" ]
 
   tags = {
     Name = "demo-server"
   }
 }
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow access -ssh 22"
+
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-sg"
+  }
+}
+
